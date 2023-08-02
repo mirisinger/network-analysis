9728d4fa8765305ffb19e914ea403b5cbe07b7f5
TamiKroizer <t8402041@gmail.com>
2023-08-02 12:40:34 +0300

    add device_controller and extract_from_db

--- Controllers/device_controller.py
+++ Controllers/device_controller.py
@@ -1,5 +1,76 @@
-def view_filter_devices(network_id, condition):
-    pass
+from DB.query import execute_query
+# from DB.extract_from_db import select_filter_devices
+
+
+# how do i get the name of the filter and the value,
+# maybe as:{ip:123.456.777}
+# maybe as:2 params (ip,1234.566.677
+mac=223,
+mac:123
+
+# def view_filter_devices(input_network_id, **kwargs):
+def view_filter_devices(input_network_id, condition=''):
+    # is_athorized
+    match condition:
+        case 'ip':
+            print("ip")
+            device_by_ip = """
+            SELECT * 
+            FROM Device
+            WHERE ip = condition
+            """
+            a = execute_query(device_by_ip).fetchall()
+            for i in a:
+                print(a[i])
+            # select_filter_devices(ip)
+        case 'mac':
+            print("mac")
+            device_by_mac = """
+                       SELECT * 
+                       FROM Device
+                       WHERE mac = condition
+                       """
+            a = execute_query(device_by_mac).fetchall()
+            for i in a:
+                print(a[i])
+            # select_filter_devices(mac)
+        case 'protocol':
+            print("protocol")
+            device_by_protocol = """
+                       SELECT * 
+                       FROM Device
+                       WHERE protocol = condition
+                       """
+            a = execute_query(device_by_protocol).fetchall()
+            for i in a:
+                print(a[i])
+            # select_filter_devices(protocol)
+        case 'vendor':
+            print("vendor")
+            device_by_vendor = """
+                               SELECT * 
+                               FROM Device
+                               WHERE mac.contains = condition
+                               """
+            a = execute_query(device_by_vendor).fetchall()
+            for i in a:
+                print(a[i])
+            # select_filter_devices(vendor or mac)
+        case _:
+            all_network_devices = """
+            SELECT * 
+            FROM Device 
+            WHERE network_id = input_network_id
+            """
+
+            #WHERE network_id = network_id
+            a = execute_query(all_network_devices).fetchall()
+            for i in a:
+                print(a[i])
+            # results = cursor.fetchall()
+            # select_filter_devices(network_id)
+
+
 
 
 # NTH
--- DB/extract_from_db.py
+++ DB/extract_from_db.py
@@ -1,9 +1,36 @@
-def get_network_dict():
-    pass
+# from DB.query import execute_query
+def get_network_dict(network_id):
+    # return {1:[2,3,4],2:[5,6]}
+
+    network_devices = f"""
+    SELECT mac 
+    FROM Device
+    WHERE network_id = {network_id}
+    """
+    network_dict = {}
+    for device in network_devices:
+        device_connections = f"""
+        SELECT dest_mac FROM Device_Connections_table
+        WHERE src_mac = {device}
+        """
+        # network_dict.append(key = device,value = device_connections:[])
+
+        network_dict[device] = [device_connections]
+        # network_dict[device] = device_connections
 
 
-def match_filter():
-    pass
+# def match_filter():
+#     pass
+def select_filter_devices(condition):
+    q = """
+    SELECT * 
+    FROM Device 
+    WHERE condition = condition
+    """
+    query_result = execute_query(q).fetchall()
+    # results = cursor.fetchall()
+    for i in query_result:
+        print(query_result[i])
 
 
 # NTH
